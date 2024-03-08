1954c460b1603099aa1a594eab64162faf366c6c
Nazmul-Hassan-10 <adornazmulhassan@gmail.com>
2024-03-08 19:31:53 +0600

    structure update

--- Header File/structure.cpp
+++ Header File/structure.cpp
@@ -32,3 +32,24 @@ int main() {
 
     return 0;
 }
+
+
+// --------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+struct students
+{
+    int age;
+    int roll;
+    double ave_mark;
+};
+
+int main(){
+    students moon = {17, 9, 95.3};
+    cout << moon.age << endl;
+    cout << moon.roll << endl;
+    cout << moon.ave_mark << endl;
+    return 0;
+}
